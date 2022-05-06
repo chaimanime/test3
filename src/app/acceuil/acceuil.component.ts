e39c4bd4c9c8e286e2ac0e3128ce4d45dff550f8
chaimabk98 <chaima.barka@sesame.com.tn>
2022-05-06 17:55:43 +0100

    First Commit

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil',
+  templateUrl: './acceuil.component.html',
+  styles: [
+  ]
+})
+export class AcceuilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
